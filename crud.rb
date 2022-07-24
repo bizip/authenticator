d7f3fd7a7d22b23e69ba1a4f1c9cabf3588c4078
bizip <bizip04@gmail.com>
2022-07-24 09:09:22 +0200

    Add a reusable module

--- crud.rb
+++ crud.rb
@@ -1,44 +1,32 @@
 require 'bundler/inline'
-
-users = [{ username: 'bizimungu', password: 'bizimungu' },
-         { username: 'pascal', password: 'pascal' },
-         { username: 'issa', password: 'issa' },
-         { username: 'fazir', password: 'fazir' },
-         { username: 'bizp', password: 'bizp' }]
-
 gemfile true do
   source 'http://rubygems.org'
   gem 'bcrypt'
 end
 
-require 'bcrypt'
+module Crud
+  require 'bcrypt'
+  puts "Module clud activated"
 
-def create_hash_digest(password)
-  BCrypt::Password.create(password)
-end
-
-def verify_hash_digest(password)
-  BCrypt::Password.new(password)
-end
-
-def create_secure_users(user_list)
-  user_list.each do |record|
-    record[:password] = create_hash_digest(record[:password])
+  def Crud.create_hash_digest(password)
+    BCrypt::Password.create(password)
   end
-  user_list
-end
 
-newPassword = create_hash_digest('password1')
-secured = create_secure_users(users)
-
-def authenticate_user(username, _password, list_of_users)
-  list_of_users.each do |record|
-  if record[:username] == username && verify_hash_digest(record[:password]) == _password
-    return record
+  def Crud.verify_hash_digest(password)
+    BCrypt::Password.new(password)
   end
-  end
-  'credentials were not correct'
-end
 
-auth=authenticate_user('bizimungu', 'bizimungu', secured)
-puts auth
+  def Crud.create_secure_users(user_list)
+    user_list.each do |record|
+      record[:password] = create_hash_digest(record[:password])
+    end
+    user_list
+  end
+
+  def Crud.authenticate_user(username, _password, list_of_users)
+    list_of_users.each do |record|
+      return record if record[:username] == username && verify_hash_digest(record[:password]) == _password
+    end
+    'credentials were not correct'
+  end
+end
--- main.rb
+++ main.rb
@@ -1,20 +1,13 @@
-require 'bundler/inline'
+require_relative 'crud'
 
-gemfile true do
-  source 'http://rubygems.org'
-  gem 'bcrypt'
-end
+# $_LOAD_PATH << "."
+# require crud
 
-require 'bcrypt'
+users = [{ username: 'bizimungu', password: 'bizimungu' },
+         { username: 'pascal', password: 'pascal' },
+         { username: 'issa', password: 'issa' },
+         { username: 'fazir', password: 'fazir' },
+         { username: 'bizp', password: 'bizp' }]
 
-my_password = BCrypt::Password.create('my password')
-#=> "$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa"
-puts my_password
-puts my_password.version #=> "2a"
-puts my_password.cost #=> 10
-puts my_password == 'my password' #=> true
-puts my_password == 'not my password' #=> false
-
-# my_password = BCrypt::Password.new("$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa")
-# my_password == "my password"     #=> true
-# my_password == "not my password" #=> false
+hashed_users= Crud.create_secure_users(users)
+puts hashed_users
